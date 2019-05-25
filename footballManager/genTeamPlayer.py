b144ac47bf218dfdd1674b7910571b0da2c37b6d
ccforever <>
2019-05-25 23:05:00 +0800

    update

--- footballManager/genTeamPlayer.py
+++ footballManager/genTeamPlayer.py
@@ -30,10 +30,10 @@ def genTeamPlayers(teamName, playerCount):
 def genGoalKeeperData(count):
     playerData = {
         "playerNumber": count,
-        "Name": randomName(),
-        "Birth": '{}.{}'.format(random.randint(1980, 2000), random.randint(1, 12)),
-        "National": 'China',
-        "Ability": {
+        "name": randomName(),
+        "birth": '{}.{}'.format(random.randint(1980, 2000), random.randint(1, 12)),
+        "national": 'China',
+        "ability": {
             "goalkeeper": random.randint(15, 20),
             "defend": random.randint(1, 10),
             "setup": random.randint(1, 10),
@@ -41,6 +41,7 @@ def genGoalKeeperData(count):
             "pass": random.randint(1, 10),
             "shoot": random.randint(1, 10),
             "freekick": random.randint(1, 15),
+            "teamwork": random.random(),
         },
         "stamina": random.randint(1, 20),
         "state": random.randint(1, 20),
@@ -52,10 +53,10 @@ def genGoalKeeperData(count):
 def genSinglePlayerData(count):
     playerData = {
         "playerNumber": count,
-        "Name": randomName(),
-        "Birth": '{}.{}'.format(random.randint(1980, 2000), random.randint(1, 12)),
-        "National": 'China',
-        "Ability": {
+        "name": randomName(),
+        "birth": '{}.{}'.format(random.randint(1980, 2000), random.randint(1, 12)),
+        "national": 'China',
+        "ability": {
             "goalkeeper": random.randint(1, 10),    # 守门能力
             "defend": random.randint(1, 20),    # 防守能力
             "setup": random.randint(1, 20), # 组织进攻能力
@@ -63,6 +64,7 @@ def genSinglePlayerData(count):
             "pass": random.randint(1, 20),  # 传球能力
             "shoot": random.randint(1, 20), # 射门能力
             "freekick": random.randint(1, 20),  # 任意球能力
+            "teamwork": random.random(),    # 团队合作概率，[0,1]
         },
         "stamina": random.randint(1, 20),   # 体能，表达球场覆盖范围
         "state": random.randint(1, 20), # 状态，表达能力加成，暂未使用
--- footballManager/matchMonitor.py
+++ footballManager/matchMonitor.py
@@ -27,36 +27,47 @@ def getTeamData(teamFile):  # 读取球队数据文件
 
 def startMatch(homeTeamData, awayTeamData): # 开始比赛模拟
     ball = [1, 1]
+    if roll() == 0:
+        attackTeam = homeTeamData['name']
+    else:
+        attackTeam = awayTeamData['name']
 
 
 
 def getFieldAbility(teamData, formation):  # 获取球队信息及阵型，并生成场上能力值， formation = [4, 4, 2]
     playerSelects = random.sample(teamData['players'][3:], 10)
     # print(playerSelects)
-    firstList = {
+    firstDict = {
         "goalkeeper": random.sample(teamData['players'][:3], 1),
         "defenders": playerSelects[:formation[0]],
         "midfielders": playerSelects[formation[0]:(formation[0]+formation[1])],
         "forwards": playerSelects[(formation[0] + formation[1]):]
     }
-    print(firstList)
-
+    firstList = [firstDict['defenders'], firstDict['midfielders'], firstDict['forwards']]
+    print('goalkeeper: {}'.format(firstDict['goalkeeper']))
+    for i in firstList:
+        print(i)
 
 
 def roll():   # roll点决定球权
     rollResult = random.random()
     if rollResult < 0.5:
-        return 0
+        return 0    # 主队持球
     else:
-        return 1
+        return 1    # 客队持球
 
 
-def pk(attackPlayer, defendPlayer):   # 球员对抗
+def pk(attackPlayer, defendPlayer):   # 球员对抗, place = [x, y]
     pass
 
 
-def action(playerData, place): # 判断球员动作, place = [x, y]
-    total = playerData['ability']['dribble'] + playerData['ability']['pass']
+def action(playerData): # 判断球员动作
+    playerAction = random.random()
+    if playerAction < playerData['ability']['teamwork']:
+        doAction = 'pass'
+    else:
+        doAction = 'dribble'
+    return doAction
 
 
 if __name__ == "__main__":
